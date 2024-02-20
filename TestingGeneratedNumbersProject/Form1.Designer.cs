3f262954657e880ad069dfd6d47f6ef388e82103
user <user@WINDOWS-58VGB5K>
2024-02-20 03:13:54 +0200

    Add project files.

---
+++ TestingGeneratedNumbersProject/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace TestingGeneratedNumbersProject
+{
+    partial class NumTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NumTest));
+            this.uniDistButton = new System.Windows.Forms.Button();
+            this.trianDistButton = new System.Windows.Forms.Button();
+            this.minValTextBox = new System.Windows.Forms.TextBox();
+            this.maxValTextBox = new System.Windows.Forms.TextBox();
+            this.genNumTextBox = new System.Windows.Forms.TextBox();
+            this.compCostTextBox = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.noRangeCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // uniDistButton
+            // 
+            this.uniDistButton.Location = new System.Drawing.Point(12, 168);
+            this.uniDistButton.Name = "uniDistButton";
+            this.uniDistButton.Size = new System.Drawing.Size(124, 43);
+            this.uniDistButton.TabIndex = 0;
+            this.uniDistButton.Text = "Generate Number";
+            this.uniDistButton.UseVisualStyleBackColor = true;
+            this.uniDistButton.Click += new System.EventHandler(this.uniDistButton_Click);
+            // 
+            // trianDistButton
+            // 
+            this.trianDistButton.Location = new System.Drawing.Point(160, 168);
+            this.trianDistButton.Name = "trianDistButton";
+            this.trianDistButton.Size = new System.Drawing.Size(124, 43);
+            this.trianDistButton.TabIndex = 1;
+            this.trianDistButton.Text = "Generate Number";
+            this.trianDistButton.UseVisualStyleBackColor = true;
+            this.trianDistButton.Click += new System.EventHandler(this.trianDistButton_Click);
+            // 
+            // minValTextBox
+            // 
+            this.minValTextBox.Location = new System.Drawing.Point(12, 47);
+            this.minValTextBox.Name = "minValTextBox";
+            this.minValTextBox.Size = new System.Drawing.Size(243, 20);
+            this.minValTextBox.TabIndex = 2;
+            // 
+            // maxValTextBox
+            // 
+            this.maxValTextBox.Location = new System.Drawing.Point(280, 47);
+            this.maxValTextBox.Name = "maxValTextBox";
+            this.maxValTextBox.Size = new System.Drawing.Size(261, 20);
+            this.maxValTextBox.TabIndex = 3;
+            // 
+            // genNumTextBox
+            // 
+            this.genNumTextBox.Location = new System.Drawing.Point(314, 139);
+            this.genNumTextBox.Name = "genNumTextBox";
+            this.genNumTextBox.Size = new System.Drawing.Size(330, 20);
+            this.genNumTextBox.TabIndex = 4;
+            // 
+            // compCostTextBox
+            // 
+            this.compCostTextBox.Location = new System.Drawing.Point(314, 191);
+            this.compCostTextBox.Name = "compCostTextBox";
+            this.compCostTextBox.Size = new System.Drawing.Size(330, 20);
+            this.compCostTextBox.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 119);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 30);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "128-bit\r\nNumber\r\n";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(84, 119);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(63, 30);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "64-bit\r\nNumber\r\n";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(153, 119);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(63, 30);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "32-bit\r\nNumber\r\n";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Min";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(311, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Generated Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(311, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Computational Cost";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Uniform Distribution";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(157, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Triangular Distribution";
+            // 
+            // noRangeCheckBox
+            // 
+            this.noRangeCheckBox.AutoSize = true;
+            this.noRangeCheckBox.Location = new System.Drawing.Point(15, 83);
+            this.noRangeCheckBox.Name = "noRangeCheckBox";
+            this.noRangeCheckBox.Size = new System.Drawing.Size(115, 30);
+            this.noRangeCheckBox.TabIndex = 15;
+            this.noRangeCheckBox.Text = "Generate Number\r\nwithout range limits";
+            this.noRangeCheckBox.UseVisualStyleBackColor = true;
+            this.noRangeCheckBox.CheckedChanged += new System.EventHandler(this.noRangeCheckBox_CheckedChanged);
+            // 
+            // NumTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(726, 291);
+            this.Controls.Add(this.noRangeCheckBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.compCostTextBox);
+            this.Controls.Add(this.genNumTextBox);
+            this.Controls.Add(this.maxValTextBox);
+            this.Controls.Add(this.minValTextBox);
+            this.Controls.Add(this.trianDistButton);
+            this.Controls.Add(this.uniDistButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "NumTest";
+            this.Text = "Generated Number Modeling";
+            this.Load += new System.EventHandler(this.NumTest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uniDistButton;
+        private System.Windows.Forms.Button trianDistButton;
+        private System.Windows.Forms.TextBox minValTextBox;
+        private System.Windows.Forms.TextBox maxValTextBox;
+        private System.Windows.Forms.TextBox genNumTextBox;
+        private System.Windows.Forms.TextBox compCostTextBox;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox noRangeCheckBox;
+    }
+}
+
---
+++ TestingGeneratedNumbersProject/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using MathNet.Numerics.Distributions;
+using System.Numerics;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestingGeneratedNumbersProject
+{
+    public partial class NumTest : Form
+    {
+        private BigInteger generatedNum;
+        private RandomNumberGenerator rng;
+        private long rangeNumMin = 0;
+        private long rangeNumMax = 0;
+        private int byteN = 0;
+        private int previousbyteN = 0;
+        private bool noRangeLimitation = false;
+        public NumTest()
+        {
+            InitializeComponent();
+            rng = RandomNumberGenerator.Create();
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            byteN = 0;
+        }
+
+        private void uniDistButton_Click(object sender, EventArgs e)
+        {
+            GenerateRandomNumber();
+        }
+
+        private void trianDistButton_Click(object sender, EventArgs e)
+        {
+            GenerateTrianDistRandomNumber();
+        }
+
+        private void NumTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                byteN = 16;
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                byteN = 8;
+                checkBox1.Checked = false;
+                checkBox3.Checked = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                byteN = 4;
+                checkBox1.Checked = false;
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void GenerateRandomNumber()
+        {
+            if (!noRangeLimitation)
+            {
+                if (string.IsNullOrEmpty(minValTextBox.Text) && string.IsNullOrEmpty(maxValTextBox.Text))
+                {
+                    MessageBox.Show("Please enter a values");
+                    return;
+                }
+                else
+                {
+                    rangeNumMin = long.Parse(minValTextBox.Text);
+                    rangeNumMax = long.Parse(maxValTextBox.Text);
+                }              
+            }
+
+            if (rangeNumMax < rangeNumMin)
+            {
+                MessageBox.Show("Max value must be greater than Min value.");
+                return;
+            }
+            byte[] bytes;
+            switch (byteN)
+            {
+                case 4:
+                    {
+                        bytes = new byte[byteN];
+                        break;
+                    }
+                case 8:
+                    {
+                        bytes = new byte[byteN];
+                        break;
+                    }
+                case 16:
+                    {
+                        bytes = new byte[byteN];
+                        break;
+                    }
+                default:
+                    {
+                        bytes = new byte[0];
+                        break;
+                    }
+            }
+
+            Stopwatch sw = Stopwatch.StartNew(); // stopwatch 
+
+            rng.GetBytes(bytes);
+
+            if (!string.IsNullOrEmpty(genNumTextBox.Text) || !string.IsNullOrEmpty(compCostTextBox.Text))
+            {
+                genNumTextBox.Text = "";
+                compCostTextBox.Text = "";
+            }
+
+            generatedNum = new BigInteger(bytes);
+
+            BigInteger mappedNumber = generatedNum;
+
+            if (!noRangeLimitation)
+            {
+                mappedNumber = MapToRange(generatedNum, rangeNumMin, rangeNumMax);
+            }
+
+            sw.Stop();// stopwatch ending point
+            genNumTextBox.Text += $"{mappedNumber}";
+            compCostTextBox.Text += $"Consumed Time = {sw.Elapsed}";
+        }
+
+        private BigInteger MapToRange(BigInteger number, long min, long max)
+        {
+            // Calculate the range of possible BigInteger values
+            BigInteger range = max - min + 1; // Add 1 to include both min and max values
+
+            // Ensure the range is positive
+            if (range <= 0)
+            {
+                throw new ArgumentException("Invalid range: max must be greater than min.");
+            }
+
+            // Calculate the modulus of the number with the range
+            BigInteger modulus = number % range;
+
+            // Ensure the modulus is positive
+            if (modulus < 0)
+            {
+                // Add the range to ensure the result is non-negative
+                modulus += range;
+            }
+
+            // Map the modulus to the specified range
+            return min + modulus;
+        }
+
+        private void noRangeCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            noRangeLimitation = false;
+            if (noRangeCheckBox.Checked)
+            {
+                noRangeLimitation = true;
+            }
+        }
+
+        private void GenerateTrianDistRandomNumber()
+        {
+            if (string.IsNullOrEmpty(minValTextBox.Text) || string.IsNullOrEmpty(maxValTextBox.Text))
+            {
+                MessageBox.Show("Please enter both minimum and maximum values.");
+                return;
+            }
+            else
+            {
+                rangeNumMin = long.Parse(minValTextBox.Text);
+                rangeNumMax = long.Parse(maxValTextBox.Text);
+            }
+
+            if (rangeNumMax < rangeNumMin)
+            {
+                MessageBox.Show("Max value must be greater than Min value.");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(genNumTextBox.Text) || !string.IsNullOrEmpty(compCostTextBox.Text))
+            {
+                genNumTextBox.Text = "";
+                compCostTextBox.Text = "";
+            }
+
+            Stopwatch sw = Stopwatch.StartNew(); // stopwatch 
+
+            double generatedNumb = GenerateTriangularRandomValue(rangeNumMin, rangeNumMax);
+
+            BigInteger mappedNumber = BigInteger.Zero;
+
+            mappedNumber = MapToRangeTrianDist(generatedNumb, rangeNumMin, rangeNumMax);
+
+            sw.Stop();// stopwatch ending point
+            genNumTextBox.Text += $"{mappedNumber}";
+            compCostTextBox.Text += $"Consumed Time = {sw.Elapsed}";
+        }
+        private double GenerateTriangularRandomValue(long min, long max)
+        {
+            // Create a triangular distribution with the specified min, max, and mode values
+            Triangular triangularDistribution = new Triangular(min, max, (min + max) / 2.0);
+
+            // Generate a random number from the triangular distribution
+            return triangularDistribution.Sample();
+        }
+
+        private BigInteger MapToRangeTrianDist(double number, long min, long max)
+        {
+            double normalizedNumber = (number - min) / (max - min);
+
+            long range = max - min;
+            return min + (BigInteger)(normalizedNumber * range); ;
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            GenerateRandomNumber();
+        }
+    }
+}
